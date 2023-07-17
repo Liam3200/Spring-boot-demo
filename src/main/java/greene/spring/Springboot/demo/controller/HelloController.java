9165d6fc4273d21db9adefbf0922911a4c23d39b
Liam Greene <wgreene@guilford.edu>
2023-07-17 14:01:58 -0400

    Converting config to YAML

--- src/main/java/greene/spring/Springboot/demo/controller/HelloController.java
+++ src/main/java/greene/spring/Springboot/demo/controller/HelloController.java
@@ -1,13 +1,17 @@
 package greene.spring.Springboot.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
     
+    @Value("${welcome.message}")
+    private String welcomeMessage;
+
     @GetMapping("/")
     public String helloWorld() {
-        return "Hello World! Testing";
+        return welcomeMessage;
     }
 }
