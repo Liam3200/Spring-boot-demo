7ed719697190780a8760f8cbe11d102619d2e7f3
Liam Greene <wgreene@guilford.edu>
2023-07-18 14:54:47 -0400

    changing the XML to YAML

--- src/main/java/greene/spring/Springboot/demo/controller/HelloController.java
+++ src/main/java/greene/spring/Springboot/demo/controller/HelloController.java
@@ -1,14 +1,14 @@
 package greene.spring.Springboot.demo.controller;
 
-import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
     
-    @Value("${welcome.message}")
-    private String welcomeMessage;
+    // @Value("${welcome.message}")
+    private String welcomeMessage = "Help! I'm trapped in a Spring Boot application!";
 
     @GetMapping("/")
     public String helloWorld() {
